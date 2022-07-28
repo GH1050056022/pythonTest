03d0b52b88c62c0da4e982f137dbbc7660af9e40
feng.i.liu <feng.i.liu@accenture.com>
2022-07-28 18:47:00 +0800

    data masking

---
+++ test/data_masking.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def data_masking(var):
+  # null return
+  if not var :
+    return var
+  count = len(var)
+  if count <= 2:
+    var = var[0] + '**'
+  else:
+    var = var[0] + '**' + var[count - 1]
+  return var
+
+var = data_masking('不问知雪')
+print(var)
