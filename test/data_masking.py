f32adac8d85474e7e80e580bd2f0f379a7956652
feng.i.liu <feng.i.liu@accenture.com>
2022-08-17 18:42:41 +0800

    data_masking add var = var.strip()

--- test/data_masking.py
+++ test/data_masking.py
@@ -5,12 +5,16 @@ def data_masking(var):
   # null return
   if not var :
     return var
+  var = var.strip()
   count = len(var)
   if count <= 2:
     var = var[0] + '*'
   else:
-    var = var[0] + '*'*(len(var)-2) + var[count - 1]
+    if count < 11:
+      var = var[0] + '*'*(len(var)-2) + var[count - 1]
+    else:
+      var = var[0] + '*'*9 + var[count - 1]
   return var
 
-var = data_masking(' Tommy Wong '.strip())
+var = data_masking(' Tommy Wong ')
 print(var)
