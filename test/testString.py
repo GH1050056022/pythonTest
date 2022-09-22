32ea59d7e4b226cd2962d834d1487f16e705a997
feng.i.liu <feng.i.liu@accenture.com>
2022-09-22 17:00:03 +0800

    add testString.py

---
+++ test/testString.py
@@ -0,0 +1,18 @@
+
+
+var = 'lucio'
+# 将第一个字母大写
+print(var.capitalize())
+
+# 如果字符串至少有一个字符并且所有字符都是字母或中文字则返回 True, 否则返回 False
+print(var.isalpha())
+
+# 如果字符串只包含数字则返回 True 否则返回 False..
+print(var.isdigit())
+
+# 截掉字符串左边的空格或指定字符。
+print(var.lstrip('l'))
+
+# 返回字符串长度
+num = len(var)
+print(num)
